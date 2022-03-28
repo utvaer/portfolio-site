4afd34abd66976725f5e672491d488d3b78550bd
Tiffany Utvaer Gasser <tiffany.u.gasser@gmail.com>
2022-03-28 21:34:18 +0200

    working contact form added

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,29 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import styles from "./ContactForm.module.css";
 import Title from "../typography/Title";
-import Button from "../buttons/Button";
 
-function ContactForm() {
+function ContactForm(props) {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_sw1vz2f",
+        "template_vkc2mlg",
+        form.current,
+        "uCITeDmFZokrnOooa"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    alert("Email sent");
+  };
   return (
     <div>
       <Title>Get in Touch</Title>
-      <form className={styles.form}>
+      <form className={styles.form} ref={form} onSubmit={sendEmail}>
         <div className={styles.input}>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" />
+          <input type="text" id="name" name="name" />
         </div>
 
         <div className={styles.input}>
           <label htmlFor="email">Email*</label>
-          <input type="text" required id="email" />
+          <input type="email" required id="email" name="email" />
         </div>
 
         <div className={styles.input}>
           <label htmlFor="message">Message*</label>
-          <textarea type="text" required id="message" rows="5" />
+          <textarea type="text" required id="message" name="message" rows="5" />
         </div>
 
-        <Button>Send</Button>
+        <button type="submit" value="Send">
+          Send
+        </button>
       </form>
     </div>
   );
